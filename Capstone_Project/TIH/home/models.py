eb2f231d6c6989f857ee59b085110320dff7e005
charan <scharan621@gmail.com>
2024-02-04 21:33:51 +0530

    updated db

--- Capstone_Project/TIH/home/models.py
+++ Capstone_Project/TIH/home/models.py
@@ -68,8 +68,8 @@ class UpvotedUser(models.Model):
 class Blog(BaseModel):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='blogs')
     title = models.CharField(max_length=50)
-    summary = models.CharField(max_length=500)
-    description=models.CharField(max_length=500)
+    summary = models.CharField(max_length=5000)
+    description=models.CharField(max_length=500000)
     main_image = models.TextField(null = True)
     upload_image = models.ImageField(upload_to="blogs", null=True, blank=True)
     # video = models.FileField(upload_to="blogs", null=True, blank=True)
