bb07e94514c97b60d41ed6b5343b9738330a5ab0
ຮ • ChαяαƝ <150104586+charan-1904@users.noreply.github.com>
2024-02-06 16:47:07 +0530

    Update settings.py

--- Capstone_Project/TIH/TIH/settings.py
+++ Capstone_Project/TIH/TIH/settings.py
@@ -89,8 +89,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://0.0.0.0.",
     "http://*",
     "http://localhost:3000",
+    "http://3.84.148.202:3000",
     "http://172.18.4.157:3000",
-    "http://54.146.10.178:3000"
+    "http://54.146.10.178:3000",
 ]
 
 ROOT_URLCONF = 'TIH.urls'
